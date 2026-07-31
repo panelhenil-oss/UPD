24b0ef59d71b3b53aba6404998b45cb5ec933449
panelhenil-oss <panelhenil@gmail.com>
2026-08-01 01:30:47 +0200

    Update link in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const NEW_LINK = "https://your-new-link-here.com";
+const NEW_LINK = "https://link-hub.net/1239053/3YvtIhLbofSo";
 
 document.getElementById("openLinkBtn").addEventListener("click", function () {
   window.open(NEW_LINK, "_blank", "noopener,noreferrer");
