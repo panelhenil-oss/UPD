355da4ba21980b911635320e1fdf2e0a5fe773eb
panelhenil-oss <panelhenil@gmail.com>
2026-08-31 08:23:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const CONFIG = {
   followLabel: "Follow Me",
 
   // Link to the video you want watched & liked
-  videoUrl: "https://youtu.be/dfpSUBsLc8g?si=jLZvowFhE8NUgHYE",
+  videoUrl: "https://youtu.be/jQ7k5djCF84",
   videoLabel: "Watch & Like",
 
   // The REAL link that gets unlocked (only revealed after both steps are done)
-  mainUrl: "https://linkvertise.com/1239053/3YvtIhLbofSo",
+  mainUrl: "https://link-center.net/1239053/l3x9HTQK9ltQ",
   mainLabel: "Open Link",
 
   // Minimum time (in seconds) that must pass FROM THE LATER OF THE TWO
