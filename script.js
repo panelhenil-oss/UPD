bed7335aa164bb2862b7f80313a0e129a60e1226
panelhenil-oss <panelhenil@gmail.com>
2026-09-07 09:06:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const CONFIG = {
   videoLabel: "Watch & Like",
 
   // The REAL link that gets unlocked (only revealed after both steps are done)
-  mainUrl: "https://linkvertise.com/1239053/3YvtIhLbofSo",
+  mainUrl: "https://link-target.net/1239053/6EwWEs3aR8t0",
   mainLabel: "Open Link",
 
   // Minimum time (in seconds) that must pass FROM THE LATER OF THE TWO
